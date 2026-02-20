84d46d3b50a01adec14a599c6b4b2e54ab3fe2bc
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-02-20 09:31:35 +0300

    revert insignificant changes

--- pkg/util/xorm/core/core.go
+++ pkg/util/xorm/core/core.go
@@ -744,8 +744,8 @@ type FilterWithArgs interface {
 // QuoteFilter filter SQL replace ` to database's own quote character
 type QuoteFilter struct{}
 
-func (s *QuoteFilter) Do(sql string, quoter Dialect, table *Table) string {
-	dummy := quoter.Quote("")
+func (s *QuoteFilter) Do(sql string, dialect Dialect, table *Table) string {
+	dummy := dialect.Quote("")
 	if len(dummy) != 2 {
 		return sql
 	}
@@ -767,7 +767,20 @@ func (s *QuoteFilter) Do(sql string, quoter Dialect, table *Table) string {
 // IdFilter filter SQL replace (id) to primary key column name
 type IdFilter struct{}
 
-func (i *IdFilter) Do(sql string, quoter Dialect, table *Table) string {
+type Quoter struct {
+	dialect Dialect
+}
+
+func NewQuoter(dialect Dialect) *Quoter {
+	return &Quoter{dialect}
+}
+
+func (q *Quoter) Quote(content string) string {
+	return q.dialect.Quote(content)
+}
+
+func (i *IdFilter) Do(sql string, dialect Dialect, table *Table) string {
+	quoter := NewQuoter(dialect)
 	if table != nil && len(table.PrimaryKeys) == 1 {
 		sql = strings.Replace(sql, " `(id)` ", " "+quoter.Quote(table.PrimaryKeys[0])+" ", -1)
 		sql = strings.Replace(sql, " "+quoter.Quote("(id)")+" ", " "+quoter.Quote(table.PrimaryKeys[0])+" ", -1)
@@ -800,7 +813,7 @@ func convertQuestionMark(sql, prefix string, start int) string {
 	return buf.String()
 }
 
-func (s *SeqFilter) Do(sql string, _ Dialect, _ *Table) string {
+func (s *SeqFilter) Do(sql string, dialect Dialect, table *Table) string {
 	return convertQuestionMark(sql, s.Prefix, s.Start)
 }
 
