25310110843b7727925e15d1cae0854ae746306b
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-02-20 09:45:38 +0300

    added test case

--- pkg/util/xorm/dialect_ydb_test.go
+++ pkg/util/xorm/dialect_ydb_test.go
@@ -291,6 +291,95 @@ func TestYdbFilters(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "",
+			in: args{
+				sql: `SELECT
+						  dashboard.id,
+						  dashboard.org_id,
+						  dashboard.uid,
+						  dashboard.title,
+						  dashboard.slug,
+						  dashboard_tag.term,
+						  dashboard.is_folder,
+						  dashboard.folder_id,
+						  dashboard.deleted,
+						  folder.uid AS folder_uid,
+						  folder.title AS folder_slug,
+						  folder.title AS folder_title,
+						  dashboard.title AS title
+						FROM (
+						  SELECT dashboard.id AS id, dashboard.title AS title
+						  FROM dashboard
+						  WHERE dashboard.org_id = ?
+							AND dashboard.uid IN (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+							AND dashboard.is_folder = true
+							AND dashboard.folder_uid IS NULL
+							AND dashboard.uid != ?
+							AND (dashboard.folder_uid != ? OR dashboard.folder_uid IS NULL)
+						  ORDER BY title ASC
+						  LIMIT 50 OFFSET 0
+						) AS ids
+						INNER JOIN dashboard ON ids.id = dashboard.id
+						LEFT OUTER JOIN folder ON folder.uid = dashboard.folder_uid AND folder.org_id = dashboard.org_id
+						LEFT OUTER JOIN dashboard_tag ON dashboard.id = dashboard_tag.dashboard_id
+						ORDER BY dashboard.title ASC`,
+				args: []any{
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					0, 1,
+				},
+			},
+			out: args{
+				sql: `SELECT
+						  dashboard.id,
+						  dashboard.org_id,
+						  dashboard.uid,
+						  dashboard.title,
+						  dashboard.slug,
+						  dashboard_tag.term,
+						  dashboard.is_folder,
+						  dashboard.folder_id,
+						  dashboard.deleted,
+						  folder.uid AS folder_uid,
+						  folder.title AS folder_slug,
+						  folder.title AS folder_title,
+						  dashboard.title AS title
+						FROM (
+						  SELECT dashboard.id AS id, dashboard.title AS title
+						  FROM dashboard
+						  WHERE dashboard.org_id = $p1
+							AND dashboard.uid IN $p2
+							AND dashboard.is_folder = true
+							AND dashboard.folder_uid IS NULL
+							AND dashboard.uid != $p3
+							AND (dashboard.folder_uid != $p4 OR dashboard.folder_uid IS NULL)
+						  ORDER BY title ASC
+						  LIMIT 50 OFFSET 0
+						) AS ids
+						INNER JOIN dashboard ON ids.id = dashboard.id
+						LEFT OUTER JOIN folder ON folder.uid = dashboard.folder_uid AND folder.org_id = dashboard.org_id
+						LEFT OUTER JOIN dashboard_tag ON dashboard.id = dashboard_tag.dashboard_id
+						ORDER BY dashboard.title ASC`,
+				args: []any{
+					sql.Named("p1", 0),
+					sql.Named("p2", []any{
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+						1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+						1, 2, 3, 4, 5, 6, 7, 8, 9,
+					}),
+					sql.Named("p3", 0),
+					sql.Named("p4", 1),
+				},
+			},
+		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			sql, args := applyFilters(tt.in.sql, tt.in.args)
